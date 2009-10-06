769c538e1ebd6242c25862f8f920573e3595bf97
Max Lapan <max.lapan@gmail.com>
2009-10-06 16:20:07 +0400

    domains tracking

--- Dom0/xenwatcher.c
+++ Dom0/xenwatcher.c
@@ -3,6 +3,8 @@
 #include <linux/proc_fs.h>
 #include <linux/timer.h>
 #include <linux/list.h>
+#include <linux/workqueue.h>
+#include <linux/spinlock.h>
 
 #include <xen/xenbus.h>
 
@@ -10,7 +12,7 @@
 #define MINOR_VERSION 1
 
 
-struct xw_domain_info_t {
+struct xw_domain_info {
 	struct list_head list;
 	int domain_id;
 	int page_ref;
@@ -21,14 +23,22 @@ struct xw_domain_info_t {
 #define XW_UPDATE_INTERVAL (10*HZ)
 
 /* timer routine */
-static void xw_update_domains (unsigned long);
+static void xw_update_tf (unsigned long);
+
+/* workqueue routine */
+static void xw_update_domains (struct work_struct *);
 
 static struct proc_dir_entry *xw_dir;
-static struct xw_domain_info *domains;
+
+static spinlock_t domains_lock = SPIN_LOCK_UNLOCKED;
+static LIST_HEAD (domains);
 
 static const char* xw_name = "xenwatcher";
 
-DEFINE_TIMER (xw_update_timer, xw_update_domains, 0, 0);
+DEFINE_TIMER (xw_update_timer, xw_update_tf, 0, 0);
+
+DECLARE_WORK (xw_update_worker, &xw_update_domains);
+
 
 
 
@@ -47,48 +57,6 @@ static int proc_calc_metrics (char *page, char **start, off_t off,
 }
 
 
-
-#if 0
-static int xw_read_data (char *page, char **start, off_t off, int count, int *eof, void *data)
-{
-	char **res;
-	unsigned int num, i, pref_len;
-	int len, domid, page_ref;
-	char *buf;
-	int buf_len = 128;
-	char *pref;
-
-	buf = kmalloc (buf_len, GFP_KERNEL);
-	if (!buf)
-		return -ENOMEM;
-
-	/* iterate over xen store */
-	res = xenbus_directory (XBT_NIL, "/local/domain", "", &num);
-	if (IS_ERR (res))
-		return PTR_ERR (res);
-
-	len = 0;
-	for (i = 0; i < num; i++) {
-		if (sscanf (res[i], "%d", &domid)) {
-			sprintf (buf, "%d/xenwatch/page_ref", domid);
-			/* get page reference */
-			pref = xenbus_read (XBT_NIL, "/local/domain", buf, &pref_len);
-			if (!IS_ERR (pref)) {
-				if (sscanf (pref, "%d", &page_ref))
-					len += dump_shared_page (page + len, domid, page_ref);
-				kfree (pref);
-			}
-		}
-	}
-
-	kfree (buf);
-	kfree (res);
-
-	return proc_calc_metrics (page, start, off, count, eof, len);
-}
-#endif
-
-
 static int xw_read_version (char *page, char **start, off_t off, int count, int *eof, void *data)
 {
 	int len;
@@ -104,21 +72,31 @@ inline void recharge_timer (void)
 }
 
 
-/* Timer routine. Iterates over all virtual machines (using XenStore) and obtains shared page
- * reference index. */
-static void xw_update_domains (unsigned long data)
+/* Timer routine. Schedules update work to be done (xenbus_XXX routines cannot be called from
+ * interrupt context) */
+static void xw_update_tf (unsigned long data)
+{
+	schedule_work (&xw_update_worker);
+	recharge_timer ();
+}
+
+
+static void xw_update_domains (struct work_struct *args)
 {
 	char **doms, *buf, *pref;
 	unsigned int c_doms, i, pref_len, domid, page_ref;
+	struct xw_domain_info *di, *di_private = NULL;
+
+	printk (KERN_INFO "Work Queue\n");
 
 	buf = kmalloc (128, GFP_KERNEL);
 	if (!buf)
-		goto error;
+		return;
 
 	/* iterate over all domains in XS */
 	doms = xenbus_directory (XBT_NIL, "/local/domain", "", &c_doms);
 	if (IS_ERR (doms))
-		goto error1;
+		goto error;
 
 	for (i = 0; i < c_doms; i++) {
 		if (sscanf (doms[i], "%u", &domid) <= 0)
@@ -128,19 +106,60 @@ static void xw_update_domains (unsigned long data)
 		pref = xenbus_read (XBT_NIL, "/local/domain", buf, &pref_len);
 		if (!IS_ERR (pref)) {
 			if (sscanf (pref, "%d", &page_ref)) {
-				/* handle ref,domain entry */
 				printk (KERN_INFO "Handle domain %u, ref %u\n", domid, page_ref);
+
+				spin_lock (&domains_lock);
+				di = domain_lookup (domid);
+				if (!di) {
+					/* new domain */
+					di = kmalloc (sizeof (struct xw_domain_info), GFP_KERNEL);
+					if (!di)
+						printk (KERN_WARNING "%s: memory allocation error\n", xw_name);
+					else {
+						di->domain_id = domid;
+						di->page_ref = page_ref;
+						INIT_LIST_HEAD (&di->list);
+					}
+				}
+				else {
+					if (di->page_ref != page_ref)
+						di->page_ref = page_ref;
+					/* remove domain from list to find deleted domains */
+					list_del_init (&di->list);
+				}
+
+				if (di) {
+					/* add domain_info into own private list to find all */
+					if (!di_private)
+						di_private = di;
+					else
+						list_add (&di->list, &di_private->list);
+				}
+				spin_unlock (&domains_lock);
 			}
 			kfree (pref);
 		}
 	}
 
+	spin_lock (&domains_lock);
+	if (!list_empty (&domains)) {
+		/* iterate over all remaining domains in list and remove their /proc entries */
+		struct list_head *p, *n;
+
+		list_for_each_safe (p, domains) {
+			di = list_entry (p, struct xw_domain_info, list);
+			/* delete proc entry */
+		}
+	}
+
+	/* move list entries on their place */
+	spin_unlock (&domains_lock);
+
 	kfree (doms);
-error1:
-	kfree (buf);
 error:
-	recharge_timer ();
+	kfree (buf);
 }
+
 
 
 static int __init xw_init (void)
@@ -152,7 +171,6 @@ static int __init xw_init (void)
 		return -EINVAL;
 	}
 
-//	create_proc_read_entry ("data", 0, xw_dir, xw_read_data, NULL);
 	create_proc_read_entry ("version", 0, xw_dir, xw_read_version, NULL);
 
 	recharge_timer ();
@@ -167,11 +185,10 @@ static void __exit xw_exit (void)
 	/* destroy timer */
 	del_timer_sync (&xw_update_timer);
 
-	/* destroy /proc/xenwatcher/data entry */
-//	remove_proc_entry ("data", xw_dir);
 	remove_proc_entry ("version", xw_dir);
 
 	/* remove all domain entries */
+
 
 	remove_proc_entry (xw_name, NULL);
 }
